2b7b4f32436c4dcaf85c410088b8f0c5019b5a8a
f8nque <gachaufrancis22@gmail.com>
2022-05-13 18:32:12 +0300

    Update settings.py

--- SavingsApp/settings.py
+++ SavingsApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4)z35ip(e)5tf1dh%38elk#bkt9ritrjucqfh&8%6m9iaw&kem'
+SECRET_KEY = '*********this is a secret key *******'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
